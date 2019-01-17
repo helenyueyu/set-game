855b191911e47e53733d3e0bc0519336d941608b
tokyosuite <helenyueyu@gmail.com>
2019-01-17 15:37:16 -0800

    Updates

---
+++ precache-manifest.dab729dda9f749d6a4ccf536abe24627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "277c105630a45cdfae69",
+    "url": "/set-game/static/css/main.401eb9db.chunk.css"
+  },
+  {
+    "revision": "277c105630a45cdfae69",
+    "url": "/set-game/static/js/main.277c1056.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/set-game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2dcdebaa407640f5f778",
+    "url": "/set-game/static/js/runtime~main.2dcdebaa.js"
+  },
+  {
+    "revision": "333c146d7050de13a2ba865721c9060e",
+    "url": "/set-game/index.html"
+  }
+];
